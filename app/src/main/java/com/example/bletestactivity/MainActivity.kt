fb83c7d4e4a7191e8d0401e93be06f9d7a51eb2d
JoDongGuk <ppusda@limefriends.com>
2021-08-24 12:26:39 +0900

    BLE 기기 연결하기

--- app/src/main/java/com/example/bletestactivity/MainActivity.kt
+++ app/src/main/java/com/example/bletestactivity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.TargetApi
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
@@ -27,135 +28,23 @@ import com.example.bletestactivity.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_ENABLE_BT = 1
-
-    // Bluetooth Device Scan을 위한 Permission 확인을 위한 변수 선언
-    private val REQUEST_ALL_PERMISSION = 2
-    private val PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-
-    // Bluetooth Device Scan을 위한 변수 선언
+    private val REQUEST_ENABLE_BT=1
+    private val REQUEST_ALL_PERMISSION= 2
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    private var bluetoothAdapter: BluetoothAdapter? = null
     private var scanning: Boolean = false
     private var devicesArr = ArrayList<BluetoothDevice>()
-    private val SCAN_PERIOD = 2000
+    private val SCAN_PERIOD = 1000
     private val handler = Handler()
-
-    // RecyclerView를 통해 스캔한 기기 리스트 보여주기 위한 변수 선언
     private lateinit var viewManager: RecyclerView.LayoutManager
-    private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+    private lateinit var recyclerViewAdapter : RecyclerViewAdapter
 
-    private var bluetoothAdapter: BluetoothAdapter? = null
-    private var binding: ActivityMainBinding? = null
+    // BLE Gatt
+    private var bleGatt: BluetoothGatt? = null
+    private var mContext:Context? = null
 
-    class RecyclerViewAdapter(private val myDataset: ArrayList<BluetoothDevice>) :
-        RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
-        class MyViewHolder(val linearView: LinearLayout) : RecyclerView.ViewHolder(linearView)
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            // create a new view
-            val linearView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.recyclerview_item, parent, false) as LinearLayout
-
-            return MyViewHolder(linearView)
-        }
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            val itemName: TextView = holder.linearView.findViewById(R.id.item_name)
-            val itemAddress: TextView = holder.linearView.findViewById(R.id.item_address)
-            itemName.text = myDataset[position].name
-            itemAddress.text = myDataset[position].address
-        }
-
-        override fun getItemCount(): Int = myDataset.size
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
-
-        // Scan Button Visibility
-        // Init BluetoothAdapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (bluetoothAdapter != null) {
-            // Device doesn't support Bluetooth
-            binding?.bleOnOffBtn?.isChecked = bluetoothAdapter?.isEnabled == false
-        }
-        binding?.bleOnOffBtn?.setOnClickListener {
-            bluetoothOnOff()
-            binding?.scanBtn?.visibility = if (binding?.scanBtn?.visibility == View.VISIBLE) {
-                View.INVISIBLE
-            } else {
-                View.VISIBLE
-            }
-        }
-
-        // bluetooth Scan Button Click Event
-        binding?.scanBtn?.setOnClickListener { v: View? ->
-            if (!hasPermission(this, PERMISSIONS))
-                requestPermissions(PERMISSIONS, REQUEST_ALL_PERMISSION)
-            scanDevice(true)
-        }
-
-        // RecyclerView
-        viewManager = LinearLayoutManager(this)
-        recyclerViewAdapter = RecyclerViewAdapter(devicesArr)
-        binding?.recyclerView?.layoutManager =viewManager
-        binding?.recyclerView?.adapter = recyclerViewAdapter
-
-    }
-
-    fun bluetoothOnOff() {
-        if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-            Log.d("BluetoothAdapter", "Device doesn't support Bluetooth")
-        } else {
-            if (bluetoothAdapter?.isEnabled == false) {// 블루투스 꺼져 있으면 => 활성화
-                val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableIntent, REQUEST_ENABLE_BT)
-            } else {
-                bluetoothAdapter?.disable()
-            }
-        }
-    }
-
-    // App에서 Permission이 제대로 등록되었는지 검사하는 함수
-    private fun hasPermission(context: Context?, permissions: Array<String>): Boolean {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
-            for (permission in permissions) {
-                if (ActivityCompat.checkSelfPermission(
-                        context,
-                        permission
-                    ) != PackageManager.PERMISSION_GRANTED
-                )
-                    return false
-            }
-        }
-        return true
-    }
-
-    // Permission 확인
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_ALL_PERMISSION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    Toast.makeText(this, "Permissions Granted!", Toast.LENGTH_SHORT).show()
-                else {
-                    requestPermissions(permissions, REQUEST_ALL_PERMISSION)
-                    Toast.makeText(this, "Permission Must Be Granted", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    // Bluetooth Device Scan 하기
     private val mLeScanCallback = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     object : ScanCallback() {
         override fun onScanFailed(errorCode: Int) {
@@ -165,13 +54,12 @@ class MainActivity : AppCompatActivity() {
 
         override fun onBatchScanResults(results: MutableList<ScanResult>?) {
             super.onBatchScanResults(results)
-            results?.let {
+            results?.let{
                 // results is not null
-                for (result in it) {
-                    if (!devicesArr.contains(result.device) && result.device.name != null) devicesArr.add(
-                        result.device
-                    )
+                for (result in it){
+                    if (!devicesArr.contains(result.device) && result.device.name!=null) devicesArr.add(result.device)
                 }
+
             }
         }
 
@@ -179,28 +67,163 @@ class MainActivity : AppCompatActivity() {
             super.onScanResult(callbackType, result)
             result?.let {
                 // result is not null
-                if (!devicesArr.contains(it.device) && it.device.name != null) devicesArr.add(it.device)
+                if (!devicesArr.contains(it.device) && it.device.name!=null) devicesArr.add(it.device)
                 recyclerViewAdapter.notifyDataSetChanged()
+            }
+        }
+
+    }
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun scanDevice(state:Boolean) = if(state){
+        handler.postDelayed({
+            scanning = false
+            bluetoothAdapter?.bluetoothLeScanner?.stopScan(mLeScanCallback)
+        }, SCAN_PERIOD)
+        scanning = true
+        devicesArr.clear()
+        bluetoothAdapter?.bluetoothLeScanner?.startScan(mLeScanCallback)
+    }else{
+        scanning = false
+        bluetoothAdapter?.bluetoothLeScanner?.stopScan(mLeScanCallback)
+    }
+
+    private fun hasPermissions(context: Context?, permissions: Array<String>): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (permission in permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    // Permission check
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_ALL_PERMISSION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permissions granted!", Toast.LENGTH_SHORT).show()
+                } else {
+                    requestPermissions(permissions, REQUEST_ALL_PERMISSION)
+                    Toast.makeText(this, "Permissions must be granted", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
 
-    /**
-     * scanDevice라는 함수를 통해서 매개변수 state가 true이면 handler를 이용해 Bluetooth Scan을 SCAN_PERIOD 동안 실행하고 false이면, Scanning을 멈춘다.
-     */
+    @TargetApi(Build.VERSION_CODES.M)
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun scanDevice(state: Boolean) = if (state) {
-        handler.postDelayed({
-            scanning = false
-            bluetoothAdapter?.bluetoothLeScanner?.stopScan(mLeScanCallback)
-        }, SCAN_PERIOD.toLong())
-        scanning = true
-        devicesArr.clear()
-        bluetoothAdapter?.bluetoothLeScanner?.startScan(mLeScanCallback)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mContext = this
+        val bleOnOffBtn:ToggleButton = findViewById(R.id.ble_on_off_btn)
+        val scanBtn: Button = findViewById(R.id.scanBtn)
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        viewManager = LinearLayoutManager(this)
 
-    } else {
-        scanning = false
-        bluetoothAdapter?.bluetoothLeScanner?.stopScan(mLeScanCallback)
+        recyclerViewAdapter =  RecyclerViewAdapter(devicesArr)
+        recyclerViewAdapter.mListener = object : RecyclerViewAdapter.OnItemClickListener{
+            override fun onClick(view: View, position: Int) {
+                scanDevice(false) // scan 중지
+                val device = devicesArr.get(position)
+                bleGatt =  DeviceControlActivity(mContext, bleGatt).connectGatt(device)
+            }
+        }
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            layoutManager = viewManager
+            adapter = recyclerViewAdapter
+        }
+
+        if(bluetoothAdapter!=null){
+            if(bluetoothAdapter?.isEnabled==false){
+                bleOnOffBtn.isChecked = true
+                scanBtn.isVisible = false
+            } else{
+                bleOnOffBtn.isChecked = false
+                scanBtn.isVisible = true
+            }
+        }
+
+        bleOnOffBtn.setOnCheckedChangeListener { _, isChecked ->
+            bluetoothOnOff()
+            scanBtn.visibility = if (scanBtn.visibility == View.VISIBLE){ View.INVISIBLE } else{ View.VISIBLE }
+            if (scanBtn.visibility == View.INVISIBLE){
+                scanDevice(false)
+                devicesArr.clear()
+                recyclerViewAdapter.notifyDataSetChanged()
+            }
+        }
+
+        scanBtn.setOnClickListener { v:View? -> // Scan Button Onclick
+            if (!hasPermissions(this, PERMISSIONS)) {
+                requestPermissions(PERMISSIONS, REQUEST_ALL_PERMISSION)
+            }
+            scanDevice(true)
+        }
+
     }
+    fun bluetoothOnOff(){
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Log.d("bluetoothAdapter","Device doesn't support Bluetooth")
+        }else{
+            if (bluetoothAdapter?.isEnabled == false) { // 블루투스 꺼져 있으면 블루투스 활성화
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            } else{ // 블루투스 켜져있으면 블루투스 비활성화
+                bluetoothAdapter?.disable()
+            }
+        }
+    }
+
+    class RecyclerViewAdapter(private val myDataset: ArrayList<BluetoothDevice>) :
+        RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+
+        var mListener : OnItemClickListener? = null
+
+        interface OnItemClickListener{
+            fun onClick(view: View, position: Int)
+        }
+
+
+        class MyViewHolder(val linearView: LinearLayout) : RecyclerView.ViewHolder(linearView)
+
+        override fun onCreateViewHolder(parent: ViewGroup,
+                                        viewType: Int): RecyclerViewAdapter.MyViewHolder {
+            // create a new view
+            val linearView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item, parent, false) as LinearLayout
+
+            return MyViewHolder(linearView)
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val itemName:TextView = holder.linearView.findViewById(R.id.item_name)
+            val itemAddress:TextView = holder.linearView.findViewById(R.id.item_address)
+            itemName.text = myDataset[position].name
+            itemAddress.text = myDataset[position].address
+            if(mListener!=null){
+                holder?.itemView?.setOnClickListener{v->
+                    mListener?.onClick(v, position)
+                }
+            }
+        }
+
+        override fun getItemCount() = myDataset.size
+    }
+}
+
+private fun Handler.postDelayed(function: () -> Unit?, scanPeriod: Int) {
+
 }
 
